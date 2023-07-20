8edcb4e07bc5d7c6161e14f77d4f0ecaf74c40f9
renatof4rias <renatof4rias@gmail.com>
2023-07-20 14:02:03 -0300

    Projeto Criado

---
+++ backend/src/test/java/com/softwarek/entregas/EntregasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwarek.entregas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
